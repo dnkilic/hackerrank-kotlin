dce6f5b7121a7aa28ef349a54e20cd1e5483a702
Dogan Kilic <dnkilic@outlook.com>
2019-04-21 12:31:41 +0700

    [HR-0421]: Solved Zig Zag Sequence problem.

---
+++ src/algorithms/ZigZagSequence.java
@@ -0,0 +1,30 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class ZigZagSequence {
+
+    public static void findZigZagSequence(int [] a, int n){
+        Arrays.sort(a);
+        int mid = n - (n + 1)/2;
+        int temp = a[mid];
+        a[mid] = a[n - 1];
+        a[n - 1] = temp;
+
+        int st = mid + 1;
+        int ed = n - 2;
+        while(st <= ed){
+            temp = a[st];
+            a[st] = a[ed];
+            a[ed] = temp;
+            st = st + 1;
+            ed = ed - 1;
+        }
+        for(int i = 0; i < n; i++){
+            if(i > 0) System.out.print(" ");
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+
+}
